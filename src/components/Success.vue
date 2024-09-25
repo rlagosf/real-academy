e37f963cb722d4acec7a3ff7c9649c17eb488f21
Rodrigo Lagos <rodrigo.lagos.fernandez@gmail.com>
2024-09-25 10:12:54 -0300

    AGREGADO ARCHIVO SUCCESS

---
+++ src/components/Success.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="success">
+    <img src="/real-academy-fc/logo-en-blanco.png" alt="Logo de la Academia" />
+    <h2>¡Formulario Enviado!</h2>
+    <p>Gracias por contactarnos. Nos pondremos en contacto contigo pronto.</p>
+    <router-link to="/">Volver al Inicio</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Success'
+};
+</script>
+
+<style scoped>
+.success {
+  text-align: center;
+  padding: 60px;
+}
+
+img {
+  max-width: 200px;
+}
+</style>
