234f5df5895deaade2e4f01bacd67ee0303e1504
Rodrigo Lagos <rodrigo.lagos.fernandez@gmail.com>
2024-10-03 09:05:49 -0300

    CAMBIOS PARA PUBLISH

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,16 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/real-academy/'  // Asegúrate de que coincida con el nombre del repositorio
+    : '/',
   
-  // Aquí agregamos la configuración de Webpack
   configureWebpack: {
     plugins: [
-      // Define las feature flags de Vue
       new (require('webpack')).DefinePlugin({
-        __VUE_OPTIONS_API__: true,           // Si estás usando la Options API
-        __VUE_PROD_DEVTOOLS__: false,        // Desactiva las devtools en producción
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false  // Desactiva el mensaje de mismatch
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
       })
     ]
   }
