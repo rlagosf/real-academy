e999560b99d082aa63c77e12e935791b210d17ca
rlagosf <rodrigo.lagos.fernandez@gmail.com>
2024-10-04 14:50:25 -0300

    cambios antes del deploy 1450

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/real-academy/'  // Asegúrate de que coincida con el nombre del repositorio
+    ? '/real-academy/' // Asegúrate de que coincida con el nombre del repositorio
     : '/',
   assetsDir: 'static', // Especifica un directorio para archivos estáticos (opcional)
   
   configureWebpack: {
     plugins: [
-      new (require('webpack')).DefinePlugin({
+      new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: true,
         __VUE_PROD_DEVTOOLS__: false,
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
@@ -38,6 +39,17 @@ module.exports = defineConfig({
       .options({
         name: '[name].[hash:8].[ext]', // Modifica esto según sea necesario
         outputPath: 'images/', // Cambia esto si tienes una carpeta específica para imágenes
+      });
+
+    // Permitir la carga de archivos estáticos en general
+    config.module
+      .rule('static-files')
+      .test(/\.(png|jpe?g|gif|svg|mp4|webm|ogg|mp3|wav|flac|aac)$/) // Añadido para cargar todos los tipos
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: '[name].[hash:8].[ext]',
+        outputPath: 'static/' // Directorio para archivos estáticos
       });
   }
 });
